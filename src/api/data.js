d698e1c85cf167daa9b57756aed1366f353ba4af
yesixuan <18373209286@163.com>
2019-02-22 16:55:43 +0800

    delete: 删除些许无用代码

--- src/api/data.js
+++
@@ -1,6 +0,0 @@
-import axios from '@/libs/axios'
-
-export const register = data => axios.post('/v1/token')(data)
-export const login = data => axios.post('/v1/token')(data)
-export const getAllHabit = data => axios.get('/v1/habit')(data)
-export const postHabit = data => axios.post('/v1/habit')(data)
